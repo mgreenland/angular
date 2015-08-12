0557383be8c6b5b83ec34ef62f82d97bd8960fbd
Misko Hevery <misko@hevery.com>
2015-08-12 13:03:28 -0700

    chore: disable dart for HTTP package
    BREAKING CHANGE
    
    Stop supporting http module in Dart. This is because Dart has a
    well developed http package which should be used by Dart
    customers instead.

--- protractor-dart2js.conf.js
+++ protractor-dart2js.conf.js
@@ -6,10 +6,12 @@ config.baseUrl = 'http://localhost:8002/';
 config.exclude.push(
   'dist/js/cjs/examples/e2e_test/sourcemap/sourcemap_spec.js',
   // TODO: remove this line when largetable dart has been added
+  'dist/js/cjs/benchmarks_external/e2e_test/http_spec.js',
+  'dist/js/cjs/benchmarks_external/e2e_test/jsonp_spec.js',
   'dist/js/cjs/benchmarks_external/e2e_test/largetable_perf.js',
   'dist/js/cjs/benchmarks_external/e2e_test/polymer_tree_perf.js',
   'dist/js/cjs/benchmarks_external/e2e_test/react_tree_perf.js'
-  
+
 );
 
 data.createBenchpressRunner({ lang: 'dart' });
